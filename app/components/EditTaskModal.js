be846dabbd5c230c03799b77b287552d44138457
Tyrael122 <kanbme@protonmail.com>
2023-06-29 00:50:27 -0300

    Bug fixes

--- app/components/EditTaskModal.js
+++ app/components/EditTaskModal.js
@@ -1,27 +1,11 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { Text, TouchableOpacity, KeyboardAvoidingView, TextInput, Modal, View } from 'react-native';
 import styles from '../static/styles/editTaskModalStyleSheet';
-import BottomSheet from '@gorhom/bottom-sheet';
-
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const EditTaskModal = (properties) => {
-  // ref
-  const bottomSheetRef = useRef<BottomSheet>(null);
-  
-  // variables
-  const snapPoints = useMemo(() => ['25%', '50%'], []);
-  
     return (
       <View style={styles.container}>
-        <BottomSheet
-          ref={bottomSheetRef}
-          index={1}
-          snapPoints={snapPoints}
-        >
-          <View style={styles.contentContainer}>
-            <Text>Awesome 🎉</Text>
-          </View>
-        </BottomSheet>
       </View>
     );
 }
